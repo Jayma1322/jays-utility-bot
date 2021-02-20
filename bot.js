77e9da8c794a81c643817b56efa686e36a73df19
Jayma1322 <macraejay5@gmail.com>
2021-02-20 21:31:36 +0000

    Signed-off-by: Jayma1322 <macraejay5@gmail.com>

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-var blacklistedids = ["287704540810182657",""]
+var blacklistedids = ["",""]
 
 bot.on('ready', () => {
 	bot.user.setActivity('for ;help!', {
